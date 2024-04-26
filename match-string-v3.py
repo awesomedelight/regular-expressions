058f1c0e3aa0bb587c77dc6d9170d2cedd2ad716
Theophilus Nasali <ae2108@coventry.ac.uk>
2024-04-26 09:39:59 +0200

    words must start with ab at very least

---
+++ match-string-v3.py
@@ -0,0 +1,14 @@
+import re
+'''string should start with a and is followed by or without b and has some othe charaters'''
+def is_compliant_string(input_str):
+    pattern = re.compile(r'^ab.*')
+    truth_value = False
+    if pattern.match(input_str):
+        truth_value = True
+    return truth_value
+st1 = 'asort'
+
+if is_compliant_string(st1):
+    print(f'The string {st1} is compliant with the rules')
+else:
+    print(f'The string {st1}  is not compliant with the rules')
